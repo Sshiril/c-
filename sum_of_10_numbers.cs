8c3fe980f4b55c32244a8e7cb2e48c3076c6e4ac
Sshiril <113382540+Sshiril@users.noreply.github.com>
2022-11-01 14:05:28 -0400

    Create sum_of_10_numbers.cs

---
+++ sum_of_10_numbers.cs
@@ -0,0 +1,16 @@
+
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+ int i=1; //initialization
+    int sum = 0;
+    for(i=1; i<11 ; i++)
+    {
+      sum = sum+i;
+     
+    }
+     Console.WriteLine("Sum of 10 numbers is {0} ",sum);
+  }
+}
+
