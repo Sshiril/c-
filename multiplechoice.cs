7fc8280fd4f004a0b4090eecc6fe4452f1c0fa73
Sshiril <113382540+Sshiril@users.noreply.github.com>
2022-11-03 13:07:49 -0400

    Create multiplechoice.cs

---
+++ multiplechoice.cs
@@ -0,0 +1,34 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("4+4 is");
+
+        Console.WriteLine("1. 8");
+        Console.WriteLine("2. 7");
+        Console.WriteLine("3. 11");
+  
+        Console.WriteLine("4. 12");
+
+        Console.WriteLine("Enter the correct choice");
+
+        int opt = Convert.ToInt32(Console.ReadLine());
+
+        if (opt == 1)
+        {
+            Console.WriteLine("Correct");
+      else
+                Console.WriteLine("Incorrect");
+        }
+
+
+
+
+
+
+
+    }
+}
