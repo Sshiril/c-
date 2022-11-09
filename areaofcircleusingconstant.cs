a6f2c99a04ce53285c88bbcab7721987e4922f8f
Sshiril <113382540+Sshiril@users.noreply.github.com>
2022-11-09 13:11:57 -0500

    Create areaofcircleusingconstant.cs

---
+++ areaofcircleusingconstant.cs
@@ -0,0 +1,13 @@
+// Area of circle using constant
+
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    const double pi= 3.14;
+    double area, radius=5.4;
+    area= pi*radius*radius;
+    Console.WriteLine ("Area of circle");
+     Console.WriteLine (area);
+  }
+}
